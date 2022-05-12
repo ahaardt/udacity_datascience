8a8838874b98a6e35be044f1d48c51cabaad0cd6
ahaardt <35078917+ahaardt@users.noreply.github.com>
2022-05-12 14:43:25 +0200

    Update run.py
    Fixed file paths

--- Project 2 - Response Pipeline/app/run.py
+++ Project 2 - Response Pipeline/app/run.py
@@ -26,11 +26,13 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('DisasterResponse', engine)
+
+
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../model.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -93,7 +95,7 @@ def go():
 
 
 def main():
-    app.run(host='0.0.0.0', port=3001, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
 
 
 if __name__ == '__main__':
